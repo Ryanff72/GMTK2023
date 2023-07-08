3d231f319283324496b210e5c2be0e7b1ba0c834
Gamer Hangout <masterolouthu@gmail.com>
2023-07-08 08:38:43 -0500

    hi

--- GMTK2023/Assets/SpawnNew.cs
+++ GMTK2023/Assets/SpawnNew.cs
@@ -9,6 +9,10 @@ public class SpawnNew : MonoBehaviour
 
     Animator anim ;
     GameObject go;
+<<<<<<< Updated upstream
+=======
+    public GameObject cauldronPoof;
+>>>>>>> Stashed changes
     public Sprite[] sp;
     public Vector3[] spsc;
 
@@ -24,6 +28,7 @@ public class SpawnNew : MonoBehaviour
     public void SpawnItem (GameObject spawnedItem)
     {
         go = spawnedItem;
+<<<<<<< Updated upstream
 
         //make for each item that can be added to the pot
         if (spawnedItem.name == "veggiePaste")
@@ -33,6 +38,13 @@ public class SpawnNew : MonoBehaviour
         }
         
 
+=======
+        if(spawnedItem.name == "veggiePaste")
+        {
+            chosenSprite = sp[0];
+            chosenVector3 = spsc[0];
+        }
+>>>>>>> Stashed changes
 
         StartCoroutine("animAndSpawn");
     }
@@ -43,6 +55,7 @@ public class SpawnNew : MonoBehaviour
         transform.GetChild(0).transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = chosenSprite;
         transform.GetChild(0).transform.GetChild(0).transform.localScale = chosenVector3;
         anim.Play("NewItem");
+        Instantiate(cauldronPoof, new Vector3(-1.34000003f, -1.98000002f, -0.00999999978f), Quaternion.Euler(-90, 0, 0));
         yield return new WaitForSeconds(1.5f);
         anim.Play("Idle");
         Instantiate(go, new Vector2(4, 1.8f), Quaternion.identity);
