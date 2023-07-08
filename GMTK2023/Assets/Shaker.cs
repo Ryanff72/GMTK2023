113dc280ddeaeff5799bffde11c8e03e96709e6a
SaadTheGlad <kakikebach1950@gmail.com>
2023-07-08 15:50:47 +0100

    commented out again!

--- GMTK2023/Assets/Shaker.cs
+++ GMTK2023/Assets/Shaker.cs
@@ -1,36 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Net.NetworkInformation;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Net.NetworkInformation;
+//using UnityEngine;
 
-public class Shaker : MonoBehaviour
-{
-    //public enum shakerStatus { Shaking, NotShaking };
-    //public shakerStatus shksts;
+//public class Shaker : MonoBehaviour
+//{
+//    //public enum shakerStatus { Shaking, NotShaking };
+//    //public shakerStatus shksts;
 
 
-    public void StateMachine()
-    {
-        switch (shk)
-        {
-            case Shaker.NotShaking:
-                goBack();
-                break;
-            case ingredientStatus.Shaking:
-                syncPosition();
-                StartCoroutine("getOldDistance", 0.05f);
-                break;
-        }
-    }
+//    public void StateMachine()
+//    {
+//        switch (shk)
+//        {
+//            case Shaker.NotShaking:
+//                goBack();
+//                break;
+//            case ingredientStatus.Shaking:
+//                syncPosition();
+//                StartCoroutine("getOldDistance", 0.05f);
+//                break;
+//        }
+//    }
 
-    //private void Update()
-    //{
-    //    StateMachine();
-    //}
+//    //private void Update()
+//    //{
+//    //    StateMachine();
+//    //}
 
-    goBack()
-    {
+//    goBack()
+//    {
 
-    }
+//    }
 
-}
+//}
