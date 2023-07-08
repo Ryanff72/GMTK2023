5aea44d05b12005254b2657a74d547980daeaf16
Gamer Hangout <masterolouthu@gmail.com>
2023-07-08 11:31:30 -0500

    hi

--- GMTK2023/Assets/Scripts/CattleController.cs
+++ GMTK2023/Assets/Scripts/CattleController.cs
@@ -15,6 +15,8 @@ public class CattleController : MonoBehaviour
     public GameObject NewItemSpawner; // cauldron
 
 
+
+
     // Start is called before the first frame update
     void Start()
     {
