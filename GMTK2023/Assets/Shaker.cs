eccd8f049d8d9ed669593319a288c1ab8a6a21a7
Thomas de Bock <thomasdebock10@gmail.com>
2023-07-08 16:30:00 +0200

    puishiy

--- GMTK2023/Assets/Shaker.cs
+++ GMTK2023/Assets/Shaker.cs
@@ -11,26 +11,21 @@ public class Shaker : MonoBehaviour
 
     public void StateMachine()
     {
-        switch (shk)
-        {
-            case Shaker.NotShaking:
-                goBack();
-                break;
-            case ingredientStatus.Shaking:
-                syncPosition();
-                StartCoroutine("getOldDistance", 0.05f);
-                break;
-        }
+       // switch (shk)
+        //{
+            //case Shaker.NotShaking:
+                //goBack();
+                //break;
+            //case ingredientStatus.Shaking:
+                //syncPosition();
+                //StartCoroutine("getOldDistance", 0.05f);
+                //break;
+        //}
     }
 
     private void Update()
     {
         StateMachine();
-    }
-
-    goBack()
-    {
-
     }
 
 }
